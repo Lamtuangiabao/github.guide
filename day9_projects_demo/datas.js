b21b41a45fd8670a46c6d493eb56f8d3942e9039
hoangnm.ndm <hoangnm.ndm@gmail.com>
2023-09-24 21:46:50 +0700

    Day 9

---
+++ day9_projects_demo/datas.js
@@ -0,0 +1,33 @@
+const datas = {
+  products: [
+    {
+      id: "1",
+      name: "san pham 1",
+      price: 200,
+      imgUrls: [
+        "https://bizweb.dktcdn.net/100/438/408/products/vay-dam-nu-van6146-hph-2-yodyvn.jpg?v=1688723359950",
+        "https://bizweb.dktcdn.net/100/438/408/products/vay-dam-nu-van6146-hph-4-yodyvn.jpg?v=1681293620207",
+      ],
+    },
+    {
+      id: "2",
+      name: "san pham 1",
+      price: 200,
+      imgUrls: [
+        "https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcRryZzNCTNXJkDDB2SRqr7ny5BXdLXT2RG7MFJHCF2OHIrJZdcaLlwuFpQc-n1uxbKcU5-UwhOlPs3HJi8_X9ikXnzaV2z1AkzulNahsOCmQb0jNY8WTFDzLXs&usqp=CAc",
+      ],
+    },
+    { id: "3", name: "san pham 1", price: 200 },
+    { id: "4", name: "san pham 1", price: 200 },
+  ],
+
+  users: [
+    {
+      userName: "hoangnm",
+      email: "hoangnm.bg@gmail.com",
+      address: "Phố Núi",
+    },
+  ],
+};
+
+export default datas;
---
+++ day9_projects_demo/main.js
@@ -0,0 +1,21 @@
+import datas from "./datas.js";
+
+// TODO: BTVN Nhóm 5
+/**
+ * 1. In ra mảng các sản phẩm có giá >=200
+ * 2. In ra mảng các sản phẩm có tên chứa từ "Váy"
+ * 3. In ra mảng products với giá tăng dần
+ */
+
+// B2: Giải phương trình bậc 2
+// ax^2 + bx + c = 0
+
+function giaiPhuongTrinhBac2(a, b, c) {
+  // B1: Kiểm tra tham số a !== 0 && typeof a,b,c === "number"
+  // B2: Nếu a == 0 và b ==0 và c != 0 => Vô nghiệm
+  // B3: Nếu a == 0 , b != 0 thì tính luôn x = -c/b
+  // B4: Tính $delta = b*b - 4*a*c
+  // B5: Kiểm tra $delta.
+  // ...
+  // return ??
+}
